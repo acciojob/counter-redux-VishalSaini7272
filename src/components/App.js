d12696fdfb358415c27b509c8a98befe34710e80
VishalSaini7272 <sainivishal156@gmail.com>
2025-12-04 17:16:39 +0530

    Integrate Redux for count management in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/actions";
 
 const App = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
   return (
     <div>
         {/* Do not remove the main div */}
+     <h1>{count}</h1>
+
+      {/* 2nd child (Increment button) */}
+      <button onClick={() => dispatch(increment())}>increment</button>
+
+      {/* 3rd child (Decrement button) */}
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+   
     </div>
   )
 }
